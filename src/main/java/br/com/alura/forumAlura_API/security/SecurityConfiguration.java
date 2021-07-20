9219c0fc90a921905eb908dc9fe6fb1e99bbac10
helde <helderrodrigues37@gmail.com>
2021-07-20 20:53:33 -0300

    (CREATE): Adicionando autenticacao para acessar os metodos bloqueados

--- src/main/java/br/com/alura/forumAlura_API/security/SecurityConfiguration.java
+++ src/main/java/br/com/alura/forumAlura_API/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package br.com.alura.forumAlura_API.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,15 +8,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private  AutenticacaoService autenticacaoService;
+
     //configuracoes de controle de acesso por autenticacao (login)
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
+        //indicando qual é a classe que tem a logica de autenticacao
+        auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
     //configuracoes de autoriazacao
@@ -24,9 +30,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         //liberando acesso para os metodos litar e detalhar
         http.authorizeRequests().antMatchers(HttpMethod.GET,"/topico/listar").permitAll()
-        .antMatchers(HttpMethod.GET,"/topico/detalhar/*").permitAll()
+        .antMatchers(HttpMethod.GET,"/topico/detalhar/*").permitAll().antMatchers(HttpMethod.POST,"/usuario/cadastrar/*").permitAll()
 
-                .anyRequest().authenticated(); //configurando o acesso dos demais metodos para ser apenas por autenticacao
+                //configurando o acesso dos demais metodos para ser apenas por autenticacao
+                .anyRequest().authenticated().and().formLogin();
 
     }
 
